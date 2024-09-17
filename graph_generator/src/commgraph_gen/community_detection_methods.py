163b0ff4acc6c86d0a32f97d3a73abb8ca5a3ff7
Valentin Schröter <vasc9380@th-wildau.de>
2024-09-17 06:20:28 +0200

    Added louvain detection.

---
+++ graph_generator/src/commgraph_gen/community_detection_methods.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import networkx as nx
+
+MAX_NODES = 1000
+
+
+community_methods_config = {
+    "edge_betweenness_partition": {
+        "params": [{"key": "number_of_sets", "type": "int", "description": "Number of communities to detect", "range": [1, MAX_NODES], "default": 2}],
+        "description": "Partition created by iteratively removing the highest edge betweenness edge (basic Girvan-Newman algorithm).",
+        "method": nx.community.edge_betweenness_partition,
+    },
+    "louvain": {
+        "params": [{"key": "resolution", "type": "float", "description": "Value of the resolution parameter", "range": [0.0, 1.0], "default": 1.0}],
+        "description": "Detects communities in the graph using the Louvain method.",
+        # "method": nx.algorithms.community.modularity_max.greedy_modularity_communities
+        "method": nx.community.louvain_communities,
+    },
+}
--- graph_generator/src/commgraph_gen/main.py
+++ graph_generator/src/commgraph_gen/main.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from commgraph_gen.comm_graph.generator import CommGraphGenerator
+from commgraph_gen.community_detection_methods import community_methods_config
 from commgraph_gen.generator_methods import generator_methods_config
 
 app = Flask(__name__)
@@ -14,71 +15,6 @@ CORS(app)
 
 MAX_NODES = 1000
 
-
-# Configuration of available methods and their parameters
-# methods_config = {
-
-
-
-#     # "r-ary_tree": {
-#     #     "params": {
-#     #         "r": {"type": "int", "description": "Branching factor of the tree", "range": [1, 10], "default": 2},
-#     #         "n": {"type": "int", "description": "Number of nodes", "range": [1, MAX_NODES], "default": 10}
-#     #     },
-#     #     "description": "Creates a full r-ary tree of n nodes.",
-#     #     "method": nx.full_rary_tree
-#     # },
-#     # "watts_strogatz": {
-#     #     "params": {
-#     #         "n": {"type": "int", "description": "Number of nodes", "range": [1, MAX_NODES], "default": 10},
-#     #         "k": {"type": "int", "description": "Each node is connected to k nearest neighbors in ring topology", "range": [1, 10], "default": 4},
-#     #         "p": {"type": "float", "description": "Probability of rewiring each edge", "range": [0.0, 1.0], "default": 0.1},
-#     #         "seed": {"type": "int", "description": "Seed for random number generator", "range": [0, 2**32 - 1], "default": None}
-#     #     },
-#     #     "description": "Generates a Watts-Strogatz small-world graph.",
-#     #     "method": nx.watts_strogatz_graph
-#     # },
-#     # "barabasi_albert": {
-#     #     "params": {
-#     #         "n": {"type": "int", "description": "Number of nodes", "range": [1, MAX_NODES], "default": 10},
-#     #         "m": {"type": "int", "description": "Number of edges to attach from a new node to existing nodes", "range": [1, 10], "default": 2},
-#     #         "seed": {"type": "int", "description": "Seed for random number generator", "range": [0, 2**32 - 1], "default": None}
-#     #     },
-#     #     "description": "Generates a Barabási-Albert preferential attachment graph.",
-#     #     "method": nx.barabasi_albert_graph
-#     # },
-#     # "random_graph": {
-#     #     "params": {
-#     #         "n": {"type": "int", "description": "Number of nodes", "range": [1, MAX_NODES], "default": 10},
-#     #         "p": {"type": "float", "description": "Probability for edge creation", "range": [0.0, 1.0], "default": 0.1},
-#     #     },
-#     #     "description": "Generates a random graph using the Erdős-Rényi model.",
-#     #     "method": nx.gnp_random_graph
-#     # },
-#     # "communication_graph": {
-#     #     "params": {
-#     #         "node_count": {"type": "int", "description": "Number of nodes", "range": [1, MAX_NODES], "default": 15},
-#     #         "seed": {"type": "int", "description": "Seed for random number generator", "range": [0, 2**32 - 1], "default": "55"},
-#     #         "pipeline_length_mu": {"type": "str", "description": "Mean of the pipeline length distribution", "default": "4"},
-#     #         "pipeline_length_deviation": {"type": "str", "description": "Standard deviation of the pipeline length distribution", "default": "4"},
-#     #         "pipeline_min_len": {"type": "str", "description": "Minimum length of the pipeline", "default": "3"},
-#     #         "forward_edge_probability": {"type": "str", "description": "Probability to generate a forward edge inside a pipeline", "default": "0.0"},
-#     #         "backward_edge_probability": {"type": "str", "description": "Probability to generate a backward edge inside a pipeline", "default": "0.0"},
-#     #         "cross_connection_probability": {"type": "str", "description": "Probability to generate a cross connection between pipelines", "default": "0.0"},
-#     #         "cross_integration_probability": {"type": "str", "description": "Probability to generate a cross integration between pipelines", "default": "0.0"},
-#     #         "diamond_probability": {"type": "str", "description": "Probability to generate a diamond connection in a pipeline", "default": "0.0"},
-#     #         "node_rewiring_probability": {"type": "str", "description": "Probability to rewire the connections of a node to another node in the same pipeline", "default": "0.0"},
-#     #     },
-#     #     "description": "Generates a random graph using the Erdős-Rényi model.",
-#     #     "method": CommGraphGenerator().generate
-#     # },
-#     "zacharys_karate_club": {
-#         "params": {},
-#         "description": "Generates the Zachary's Karate Club graph.",
-#         "method": nx.karate_club_graph
-#     }
-
-# }
 
 @app.route('/generate/methods', methods=['GET'])
 def get_methods():
@@ -125,20 +61,6 @@ def generate_graph(generator):
     return jsonify(data)
 
 
-community_methods_config = {
-    "edge_betweenness_partition": {
-        "params": [
-            {"key": "number_of_sets", "type": "int", "description": "Number of communities to detect", "range": [1, MAX_NODES], "default": 2}
-        ],
-        "description": "Partition created by iteratively removing the highest edge betweenness edge (basic Girvan-Newman algorithm).",
-        "method": nx.community.edge_betweenness_partition
-    },
-    # "louvain": {
-    #     "params": {},
-    #     "description": "Detects communities in the graph using the Louvain method.",
-    #     "method": nx.algorithms.community.modularity_max.greedy_modularity_communities
-    # }
-}
 
 @app.route('/analyze/communities/methods', methods=['GET'])
 def get_community_methods():
